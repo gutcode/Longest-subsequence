34c046bcf8fda0661f44493ccde399472b7b7383
Daniel Dahal <1991baburamdahal@gmail.com>
2019-11-16 13:14:19 +0100

    Update longestSubsequence.js

--- longestSubsequence.js
+++ longestSubsequence.js
@@ -1,11 +1,12 @@
-function longestSubseq(s1, s2, s1StartIdx = 0, s2StartIDx = 0, memo) {
+<script>
+function longestSubseq(s1, s2, s1StartIdx = 0, s2StartIdx = 0, memo) {
   if (s1StartIdx >= s1.length || s2StartIdx >= s2.lenght)
 return '';
 
 // console.log('s1StartIdx', s1StartIdx, memo);
 
 if (memo == null) {
-  memo = (new Arrary(s1,length))
+  memo = (new Array(s1,length))
   memo.fill(null);
   memo = memo.map(row => (new Array(s2.length)));
 } else if (memo[s1StartIdx][s2StartIdx] != null) {
@@ -14,7 +15,7 @@ if (memo == null) {
 const result = [];
 for (let s1Idx = s1StartIdx; s1Idx < s1.length; s1Idx++) {
      const s1Char = s1[s1Idx];
-     const s2Idx = s2,indexOf(s1Char, s2StartIdx);
+     const s2Idx = s2.indexOf(s1Char, s2StartIdx);
      let result = [];
      if (s2Idx !== -1) {
         result.push(s1Char, ...longestSubseq(s1, s2, s1Idx + 1, s2Idx +1, memo))
@@ -40,4 +41,4 @@ for (let s1Idx = s1StartIdx; s1Idx < s1.length; s1Idx++) {
   return longest;
  }
  console.log('result', longestSubseq("ABAZDC", "BACBAD"))
- 
+</script>
